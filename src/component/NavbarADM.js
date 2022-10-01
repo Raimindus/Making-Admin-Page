51b0011b3bfcab667a10e4fa32b1d80346ae8a9d
stevenfernandop <nandop154@gmail.com>
2022-10-02 00:05:09 +0700

    fix warna tombol search dan buat tombol menu(masih gambar)

--- src/component/NavbarADM.js
+++ src/component/NavbarADM.js
@@ -10,6 +10,7 @@ import iconDashb from "../assets/img/dashb.png";
 import iconCar from "../assets/img/fi_truck.png";
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
+import toggleIcon from "../assets/img/fi_menu.png";
 import { Link } from "react-router-dom";
 
 function NavbarADM() {
@@ -19,33 +20,19 @@ function NavbarADM() {
         <Container fluid>
           <div className="divlogo1"></div>
           <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: "100px" }}
-              navbarScroll
-            >
+            <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: "100px" }} navbarScroll>
               <Navbar.Toggle aria-controls="navbarScroll" />
+              <img src={toggleIcon} style={{ marginLeft: "190px" }}></img>
               {/* <Nav.Link href="#action1">Home</Nav.Link>
               <Nav.Link href="#action2">Link</Nav.Link>
                */}
             </Nav>
             <Form className="d-flex">
-              <Form.Control
-                type="search"
-                placeholder="Search"
-                className="me-2 "
-                aria-label="Search"
-              />
-              <Button variant="outline-primary" style={{ marginLeft: "-10px" }}>
-                Search
-              </Button>
+              <Form.Control type="search" placeholder="Search" className="me-2 " aria-label="Search" />
+              <Button style={{ marginLeft: "-10px", backgroundColor: "white", borderColor: "#0D28A6", color: "#0D28A6" }}>Search</Button>
             </Form>
             <img src={iconAccount} className="rounded ms-3 "></img>
-            <DropdownButton
-              id="dropdown-item-button"
-              title="Unis Badri"
-              variant="light"
-            >
+            <DropdownButton id="dropdown-item-button" title="Unis Badri" variant="light">
               <Dropdown.ItemText>Unis Badri</Dropdown.ItemText>
             </DropdownButton>
           </Navbar.Collapse>
@@ -53,11 +40,7 @@ function NavbarADM() {
       </Navbar>
       <div className="d-flex column">
         <div className="side">
-          <img
-            src={iconBox}
-            className="ms-3 mt-3 "
-            style={{ marginBottom: "10px" }}
-          ></img>
+          <img src={iconBox} className="ms-3 mt-3 " style={{ marginBottom: "10px" }}></img>
           <div className=" pt-3  bg-primary">
             <Link to="/dashboard">
               <img src={iconDashb} className="iconAdm"></img>
