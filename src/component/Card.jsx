45dd483b5fa168b75036183dcf7610227455665f
Raimindus <draimindus@gmail.com>
2022-10-26 23:20:55 +0700

    fix css

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -42,7 +42,7 @@ const carCard = ({ id, image, name, category, price, updatedAt }) => {
         className="cardImg"
         alt="mobilImg"
         src={image || imagePlaceholder}
-        style={{ maxHeight: "250px" }}
+        style={{ maxHeight: "300px" }}
       />
       <Card.Body>
         <Card.Title>
